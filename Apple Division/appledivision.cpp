2332732e1cc2ea21503fb0cf132e6a1569c6daa7
Aldo Navarrete <aaldiitoo@gmail.com>
2021-09-08 15:46:40 -0500

    Apple Division

---
+++ Apple Division/appledivision.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie (0);
+    int nEntradas;
+    cin>>nEntradas;
+    for (int i=0; i<nEntradas; i++)
+    {
+        cin >> t;
+        if (t % k == 0)
+            cnt++;
+    }
+    return 0;
+}
