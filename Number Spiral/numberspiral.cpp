3019a2ef80f14b8b83da9e74f38763b02f138577
Aldo Navarrete <aaldiitoo@gmail.com>
2021-09-10 08:23:26 -0500

    Nuevo commit

--- Number Spiral/numberspiral.cpp
+++ Number Spiral/numberspiral.cpp
@@ -1,1 +1,27 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
 
+int main(){
+
+    ll n;
+    cin>>n;
+    while(n--){ //Bajando lineas
+        ll r,c;
+        cin>>r >>c;
+        ll z = max(r,c);
+        ll vExtremo = (z-1)*(z-1);
+        if(z%2 == 0){
+            // maximos izq
+            cout<<vExtremo-(c-1)<<endl;
+        }else{
+            //Par, maximos arriba
+            cout<<vExtremo-(r-1)<<endl;
+        }
+
+    }
+
+
+
+    return 0;
+}
