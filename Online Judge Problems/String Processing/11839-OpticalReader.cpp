8dc04028cd13403e2ea0b996ce22f508bdf60f0c
Aldo Navarrete <aaldiitoo@gmail.com>
2021-09-21 20:53:33 -0500

    Optical Reader

---
+++ Online Judge Problems/String Processing/11839-OpticalReader.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+/*******  All Required define Pre-Processors and typedef Constants *******/
+#define SCD(t) scanf("%d",&t)
+#define SCLD(t) scanf("%ld",&t)
+#define SCLLD(t) scanf("%lld",&t)
+#define SCC(t) scanf("%c",&t)
+#define SCS(t) scanf("%s",t)
+#define SCF(t) scanf("%f",&t)
+#define SCLF(t) scanf("%lf",&t)
+#define PD(t) printf("%d",t)
+#define PLD(t) printf("%ld",t)
+#define PLLD(t) printf("%lld",t)
+#define PC(t) printf("%c",c)
+#define PS(t) printf("%s",t)
+#define PF(t) printf("%f",t)
+#define PLF(t) printf("%lf",t)
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define all(cont) cont.begin(), cont.end()
+#define rall(cont) cont.end(), cont.begin()
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define FO(i, b) for(int  i = 0; i<b; i++)
+#define IN(A, B, C) assert( B <= A && A <= C)
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI acos(-1.0)
+#define MOD 1000000007
+#define read(type) readInt<type>()
+#define TR(v,arr) for (auto& (v): (arr))
+#define endl '\n'
+const double pi=acos(-1.0);
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+
+/**************************************/
+/******** User-defined Function *******/
+/**************************************/
+int solve(){
+    int respuesta = -1,blancos =1,actualNumber;
+    FO(i,5){
+        scanf("%d",&actualNumber);
+        if(actualNumber <= 127){ //Negro
+            if(respuesta != -1)//Ya habia otra respuesta
+                blancos++;
+            else respuesta = i; ;
+        }
+    }
+    if(blancos!=1)return -1;
+    return respuesta;
+}
+
+/********** Main()  function **********/
+int main()
+{
+    // freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+
+    int T;
+    
+    while(true) {
+        SCD(T);
+        if(T==0) return 0;
+        FO(tc,T){
+            int respuesta = solve();
+            printf("%c\n",(respuesta==0)? 'A' : (respuesta==1)? 'B':(respuesta==2)? 'C':(respuesta==3)?'D':(respuesta==4)?'E': '*');
+        }
+
+    }
+    return 0;
+}
