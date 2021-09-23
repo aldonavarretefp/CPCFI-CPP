a82308893f354186a74dfc69ac06d61c135b0576
Aldo Navarrete <aaldiitoo@gmail.com>
2021-09-23 10:52:25 -0500

    RailWay

---
+++ Online Judge Problems/Geometry/920_SunnyMountains.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+/*******  All Required define Pre-Processors and typedef Constants *******/
+#define SCD(t) scanf("%d",&t)
+#define SCLD(t) scanf("%ld",&t)
+#define SCLLD(t) scanf("%lld",&t)
+#define SCC(t) scanf("%c",&t)
+#define SCS(t) scanf("%s",t)
+#define SCF(t) scanf("%f",&t)
+#define SCLF(t) scanf("%lf",&t)
+#define PD(t) printf("%d",t)
+#define PLD(t) printf("%ld",t)
+#define PLLD(t) printf("%lld",t)
+#define PC(t) printf("%c",c)
+#define PS(t) printf("%s",t)
+#define PF(t) printf("%f",t)
+#define PLF(t) printf("%lf",t)
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define all(cont) cont.begin(), cont.end()
+#define rall(cont) cont.end(), cont.begin()
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define FO(i, b) for(int  i = 0; i<b; i++)
+#define IN(A, B, C) assert( B <= A && A <= C)
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI acos(-1.0)
+#define MOD 1000000007
+#define read(type) readInt<type>()
+#define TR(v,arr) for (auto& (v): (arr))
+#define endl '\n'
+const double pi=acos(-1.0);
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+
+/**************************************/
+/******** User-defined Function *******/
+/**************************************/
+
+struct point { double x, y;   // only used if more precision is needed
+  point() { x = y = 0.0; }                      // default constructor
+  point(double _x, double _y) : x(_x), y(_y) {}        // user-defined
+  bool operator < (point other) const { // override less than operator
+    if (fabs(x - other.x) > EPS)                 // useful for sorting
+      return x < other.x;          // first criteria , by x-coordinate
+    return y < other.y; }          // second criteria, by y-coordinate
+  // use EPS (1e-9) when testing equality of two floating points
+  bool operator == (point other) const {
+   return (fabs(x - other.x) < EPS && (fabs(y - other.y) < EPS)); } };
+
+double dist(point p1, point p2) {                // Euclidean distance
+                      // hypot(dx, dy) returns sqrt(dx * dx + dy * dy)
+  return hypot(p1.x - p2.x, p1.y - p2.y); }           // return double
+
+struct line { double a, b, c; };
+void pointsToLine(point p1, point p2, line &l) {
+  if (fabs(p1.x - p2.x) < EPS) {              // vertical line is fine
+    l.a = 1.0;   l.b = 0.0;   l.c = -p1.x;           // default values
+  } else {
+    l.a = -(double)(p1.y - p2.y) / (p1.x - p2.x);
+    l.b = 1.0;              // IMPORTANT: we fix the value of b to 1.0
+    l.c = -(double)(l.a * p1.x) - p1.y;
+} }
+
+bool areParallel(line l1, line l2) {       // check coefficients a & b
+  return (fabs(l1.a-l2.a) < EPS) && (fabs(l1.b-l2.b) < EPS); }
+
+// returns true (+ intersection point) if two lines are intersect
+bool areIntersect(line l1, line l2, point &p) {
+  if (areParallel(l1, l2)) return false;            // no intersection
+  // solve system of 2 linear algebraic equations with 2 unknowns
+  p.x = (l2.b * l1.c - l1.b * l2.c) / (l2.a * l1.b - l1.a * l2.b);
+  // special case: test for vertical line to avoid division by zero
+  if (fabs(l1.b) > EPS) p.y = -(l1.a * p.x + l1.c);
+  else                  p.y = -(l2.a * p.x + l2.c);
+  return true; }
+
+/********** Main()  function **********/
+int main()
+{
+    point P1(1100,1200),P2(1210.95,793.1815);
+    double dis = dist(P1,P2);
+
+    line l1,l2;
+    pointsToLine(point(1500,800),point(0,800),l1);
+    pointsToLine(P1,P2,l2);
+    
+    point pAux;
+    bool res = areIntersect(l1, l2, pAux); 
+    printf("l1 & l2 are intersect? %d, at (%.2lf, %.2lf)\n", res, pAux.x, pAux.y);
+    printf("Dist = %f\n",dis);
+    printf("Dist = %f\n",dist(P1,pAux));
+    return 0;
+}
