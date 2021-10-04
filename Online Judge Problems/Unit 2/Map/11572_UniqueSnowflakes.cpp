a7ec0e5a88044e10aebe6ba97768ea021d6aab39
Aldo Navarrete <aaldiitoo@gmail.com>
2021-10-04 13:12:54 -0500

    11572 Uva

---
+++ Online Judge Problems/Unit 2/Map/11572_UniqueSnowflakes.cpp
@@ -0,0 +1,200 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+/*******  All Required define Pre-Processors and typedef Constants *******/
+#define SCD(t) scanf("%d",&t)
+#define SCLD(t) scanf("%ld",&t)
+#define SCLLD(t) scanf("%lld",&t)
+#define SCC(t) scanf("%c",&t)
+#define SCS(t) scanf("%s",t)
+#define SCF(t) scanf("%f",&t)
+#define SCLF(t) scanf("%lf",&t)
+#define PD(t) printf("%d",t)
+#define PLD(t) printf("%ld",t)
+#define PLLD(t) printf("%lld",t)
+#define PC(t) printf("%c",c)
+#define PS(t) printf("%s",t)
+#define PF(t) printf("%f",t)
+#define PLF(t) printf("%lf",t)
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define all(cont) cont.begin(), cont.end()
+#define rall(cont) cont.end(), cont.begin()
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define FO(i, b) for(int  i = 0; i<b; i++)
+#define IN(A, B, C) assert( B <= A && A <= C)
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI acos(-1.0)
+#define MOD 1000000007
+#define read(type) readInt<type>()
+#define TR(v,arr) for (auto& (v): (arr))
+#define endl '\n'
+#define fastIO cin.tie(0); cout.tie(0);
+const double pi=acos(-1.0);
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef queue<int> QI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+void solve(){
+    int n; SCD(n);
+    map<int,int> m;
+    int k;
+    FO(i,n){
+        SCD(k);
+        m[k] ++;
+        debug(m);
+    }
+    std::vector<pair<int,int>> vec;
+    // copy key-value pair<int,int>s from the map to the vector
+    std::copy(m.begin(),
+            m.end(),
+            std::back_inserter<std::vector<pair<int,int>>>(vec));
+ 
+    // sort the vector by increasing the order of its pair<int,int>'s second value
+    // if the second value is equal, order by the pair<int,int>'s first value
+    std::sort(vec.begin(), vec.end(),
+            [](const pair<int,int> &l, const pair<int,int> &r)
+            {
+                if (l.second != r.second) {
+                    return l.second > r.second;
+                }
+ 
+                return l.first > r.first;
+            });
+ 
+    // // print the vector
+    // for (auto const &pair: vec) {
+    //     std::cout << '{' << pair.first << "," << pair.second << '}' << endl;
+    // }
+    // cout<< v[0] << endl;
+    int i = 0;
+    cout << vec[i].second << endl;
+    
+    // int max = maximo(m,)
+    // printf("maximo:\n");
+}
+
+void setIO(){
+  string file = __FILE__;
+  file = string(file.begin(),file.end()-3);
+  string input_file = file + "in";
+  string output_file = file + "out";
+  freopen(input_file.c_str(), "r",stdin);
+//   freopen(output_file.c_str(),"w",stdout);
+  
+}
+
+/********** Main()  function **********/
+int main()
+{
+    
+    if(getenv("CP_IO")){setIO();}
+    int nCasos;
+	cin >> nCasos;
+
+	while(nCasos--)
+	{
+		map<int, int> snowflakes;
+		int bolsas;
+		int paqueteMaximo = 0;
+
+		int paqueteRepetido = 0;
+		int tamanio = 0;
+		cin >> bolsas;
+
+		for(int i = 1; i <= bolsas; ++i)
+		{
+			int n;
+			cin >> n;
+			int valorenSnow = snowflakes[n];
+            // debug(valorenSnow);
+            // debug(snowflakes);
+            // debug(tamanio);debug(paqueteRepetido);
+            // debug(paqueteMaximo);
+			
+			if(valorenSnow) //Si existe valor en ese snowflake
+			{
+				paqueteRepetido = max(paqueteRepetido, valorenSnow);
+				tamanio = i - paqueteRepetido -1 ;
+			}
+
+			tamanio++;
+			snowflakes[n] = i;
+			paqueteMaximo = max(paqueteMaximo, tamanio);
+            // debug(valorenSnow);
+            // debug(snowflakes);
+            // debug(tamanio);debug(paqueteRepetido);
+            // debug(paqueteMaximo);
+            // PS("====\n");
+		}
+
+
+		cout << paqueteMaximo << "\n";
+	}
+
+	return 0;
+}
