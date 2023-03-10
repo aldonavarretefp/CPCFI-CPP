3a1a6c87b6202e6361785eb08950093d99ef51a8
Aldo Navarrete <aaldiitoo@gmail.com>
2023-03-09 18:14:38 -0600

    ICPC Gran Premio de Mexico 3ra Fecha

---
+++ Contests/2020 ICPC Gran Premio de Mexico 3ra Fecha/L.cpp
@@ -0,0 +1,224 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <unordered_set>
+
+using namespace std;
+
+/*******  All Required define Pre-Processors and typedef Constants *******/
+#define SCD(t) scanf("%d",&t)
+#define SCLD(t) scanf("%ld",&t)
+#define SCLLD(t) scanf("%lld",&t)
+#define SCC(t) scanf("%c",&t)
+#define SCS(t) scanf("%s",t)
+#define SCF(t) scanf("%f",&t)
+#define SCLF(t) scanf("%lf",&t)
+#define PD(t) printf("%d",t)
+#define PLD(t) printf("%ld",t)
+#define PLLD(t) printf("%lld",t)
+#define PC(t) printf("%c",c)
+#define PS(t) printf("%s",t)
+#define PF(t) printf("%f",t)
+#define PLF(t) printf("%lf",t)
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define all(cont) cont.begin(), cont.end()
+#define rall(cont) cont.end(), cont.begin()
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define FO(i, b) for(int  i = 0; i<b; i++)
+#define IN(A, B, C) assert( B <= A && A <= C)
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI acos(-1.0)
+#define MOD 1000000007
+#define read(type) readInt<type>()
+#define TR(v,arr) for (auto& (v): (arr))
+#define endl '\n'
+#define fastIO cin.tie(0); cout.tie(0);
+const double pi=acos(-1.0);
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef queue<int> QI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+struct Node {
+    bool isEnd;
+    char c;
+    vector<Node*> next;
+    Node(char c): next(vector<Node*>(26)), c(c), isEnd(false) {};
+
+};
+
+struct Trie {
+    vector<Node*> v;
+    int words;
+    
+    Trie(): v(vector<Node*> (26)), words(0){};
+
+    bool _search(string &s,Node* curr, int index){
+        if(!curr){
+            return false;
+        }
+
+        if(curr->c != s[index]){
+            return false;
+        }
+
+        if(index == s.size()-1 ){
+            if(curr->isEnd){
+                return true;
+            }else{
+                return false;
+            }
+        }
+        
+        TR(el,curr->next){
+            if(_search(s, el, index+1))
+                return true;
+        }
+        return false;
+    }
+
+    bool search(string &s){
+        Node *start = v[s[0]-'a'];
+        return _search(s, start, 0);
+    }
+
+    Node* _insert(string &s, int index , Node* curr){
+        if(curr == nullptr){
+            curr = new Node(s[index]); 
+        }
+        
+        if(index == s.size() - 1){
+                curr->isEnd = true;
+            return curr;
+        }
+            
+
+        Node* next = curr->next[s[index + 1] - 'a'];
+
+        curr -> next[s[index + 1] - 'a'] = _insert(s, index + 1, next);
+        return curr;
+
+    }
+    void insert(string &s){
+        Node* next = v[s[0] - 'a'];
+        v[s[0] - 'a'] = _insert(s, 0, next);
+        this->words += 1;
+    }
+
+};
+
+void solve(){
+    int n; cin>>n;
+    vector<string> v(n);
+    FO(i,n){
+        cin>>v[i];
+    }
+    unordered_set<string> t;
+    t.insert(v[0]);
+    FOR(i,1,n,1){
+        string s1 = v[i];
+        string reverse = s1;
+        
+        bool found = false;
+        FO(offset, s1.size()){
+            FO(j,s1.size()){
+                reverse[j] = s1[(j+offset)%s1.size()];
+            }
+            debug(reverse);
+            if(t.count(reverse)){
+                found = true;
+                break;
+            };
+        }        
+        if(found){
+            continue;
+        }else{
+            t.insert(s1);
+        }
+    }
+
+    cout << t.size() << endl;
+
+}
+
+void setIO(){
+    string file = __FILE__;
+    file = string(file.begin(),file.end()-3);
+    string input_file = file + "in";
+    string output_file = file + "out";
+    freopen(input_file.c_str(), "r",stdin);
+    freopen(output_file.c_str(),"w",stdout);
+}
+
+/********** Main()  function **********/
+int32_t main(){
+    
+    if(getenv("CP_IO")){setIO();}
+    fastIO;
+    int T=1;
+    FO(tc,T)
+        solve();
+    return 0;
+}
