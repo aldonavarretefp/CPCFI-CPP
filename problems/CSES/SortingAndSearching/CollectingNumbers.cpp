cd19c779becd17353959d19a26767e8ca1018e43
Aldo Navarrete <aaldiitoo@gmail.com>
2023-03-26 17:34:00 -0600

    CollectingNumbers

---
+++ problems/CSES/SortingAndSearching/CollectingNumbers.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <set>
+
+using namespace std;
+
+/*******  All Required define Pre-Processors and typedef Constants *******/
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define all(cont) cont.begin(), cont.end()
+#define rall(cont) cont.end(), cont.begin()
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define FO(i, b) for(int  i = 0; i<b; i++)
+#define IN(A, B, C) assert( B <= A && A <= C)
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI acos(-1.0)
+#define MOD 1000000007
+#define read(type) readInt<type>()
+#define TR(v,arr) for (auto& (v): (arr))
+#define endl '\n'
+#define fastIO cin.tie(0); cout.tie(0);
+const double pi=acos(-1.0);
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef queue<int> QI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+void solve(){
+    int n;
+    cin >> n;
+    VI v(n), idx(n);
+    FO(i, n){
+        cin >> v[i]; v[i]--;
+        idx[v[i]] = i;
+    }
+    debug(v, idx);
+    int ans = 1;
+    FO(i, n-1){
+        int curr = idx[i], next = idx[i+1];
+        if(next < curr) {
+            ans ++;
+        }
+    }
+
+    cout << ans << endl;
+}
+
+void setIO(){
+    string file = __FILE__;
+    file = string(file.begin(),file.end()-3);
+    string input_file = file + "in";
+    string output_file = file + "out";
+    freopen(input_file.c_str(), "r",stdin);
+    freopen(output_file.c_str(),"w",stdout);
+}
+
+/********** Main()  function **********/
+int32_t main(){
+    
+    if(getenv("CP_IO")){setIO();}
+    fastIO;
+    int T=1;
+    FO(tc,T)
+        solve();
+    return 0;
+}
